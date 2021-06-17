7be7cb08aee4cf094110d45a5701abbb91130f72
ZhaoWei Xu <paldier@hotmail.com>
2021-06-17 19:43:00 +0800

    add toolchains

--- asuswrt.sh
+++ asuswrt.sh
@@ -2,6 +2,8 @@
 
 
 sudo mkdir -p /opt/toolchains/
+sudo ln -sf $(pwd)/crosstools-aarch64-gcc-5.3-linux-4.1-glibc-2.22-binutils-2.25 /opt/toolchains/
+sudo ln -sf $(pwd)/crosstools-arm-gcc-5.3-linux-4.1-glibc-2.22-binutils-2.25 /opt/toolchains/
 sudo ln -sf $(pwd)/crosstools-aarch64-gcc-5.3-linux-4.1-glibc-2.24-binutils-2.25 /opt/toolchains/
 sudo ln -sf $(pwd)/crosstools-aarch64-gcc-5.5-linux-4.1-glibc-2.26-binutils-2.28.1 /opt/toolchains/
 sudo ln -sf $(pwd)/crosstools-arm-gcc-5.3-linux-4.1-glibc-2.24-binutils-2.25 /opt/toolchains/
