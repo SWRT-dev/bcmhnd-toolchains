562f359cdde62cc2393b7d3ea303c80f9dc68a5c
ZhaoWei Xu <paldier@hotmail.com>
2024-05-18 19:41:20 +0800

    add new toolchain for be chips

--- README.md
+++ README.md
@@ -21,6 +21,10 @@
     `sudo ln -sf $(pwd)/crosstools-aarch64-gcc-9.2-linux-4.19-glibc-2.30-binutils-2.32 /opt/toolchains/`
 
     `sudo ln -sf $(pwd)/crosstools-arm-gcc-9.2-linux-4.19-glibc-2.30-binutils-2.32 /opt/toolchains/`
+
+    `sudo ln -sf $(pwd)/crosstools-arm_softfp-gcc-10.3-linux-4.19-glibc-2.32-binutils-2.36.1 /opt/toolchains/`
+
+    `sudo ln -sf $(pwd)/crosstools-aarch64-gcc-10.3-linux-4.19-glibc-2.32-binutils-2.36.1 /opt/toolchains/`
     
     `sudo mkdir -p /projects/`
     
--- asuswrt.sh
+++ asuswrt.sh
@@ -11,6 +11,8 @@ sudo ln -sf $(pwd)/crosstools-arm-gcc-5.5-linux-4.1-glibc-2.26-binutils-2.28.1 /
 sudo ln -sf $(pwd)/crosstools-gcc-5.3-linux-4.1-uclibc-1.0.12-glibc-2.24-binutils-2.25 /opt/toolchains/
 sudo ln -sf $(pwd)/crosstools-aarch64-gcc-9.2-linux-4.19-glibc-2.30-binutils-2.32 /opt/toolchains/
 sudo ln -sf $(pwd)/crosstools-arm-gcc-9.2-linux-4.19-glibc-2.30-binutils-2.32 /opt/toolchains/
+sudo ln -sf $(pwd)/crosstools-arm_softfp-gcc-10.3-linux-4.19-glibc-2.32-binutils-2.36.1 /opt/toolchains/
+sudo ln -sf $(pwd)/crosstools-aarch64-gcc-10.3-linux-4.19-glibc-2.32-binutils-2.36.1 /opt/toolchains/
 sudo mkdir -p /projects/
 sudo mkdir -p /projects/bca/
 sudo mkdir -p /projects/bca/tools/
